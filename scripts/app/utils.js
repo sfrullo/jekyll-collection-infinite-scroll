6d12efb761fe9a5b870405f78c7fd444771eb6b2
Simone <simone.castellani@nttdata.com>
2017-10-27 00:08:44 +0200

    added append method

--- scripts/app/utils.js
+++ scripts/app/utils.js
@@ -1,17 +1,28 @@
-define(	function () {
-	
-	return {
-		
-		_load : function(what, where) {
-			var xhr= new XMLHttpRequest();
-			xhr.open('GET', what, true);
-			xhr.onreadystatechange= function() {
-			    if (this.readyState!==4) return;
-			    if (this.status!==200) return; // or whatever error handling you want
-			    document.getElementById(where).innerHTML= this.responseText;
-			};
-			xhr.send();
-		}
+define( function () {
 
-	}
+    return {
+
+        __append : function (what, where, callback) {
+            var element = document.getElementById(where);
+            var __old_html = element.innerHTML;
+            element.innerHTML = __old_html + what;
+            if (callback!==undefined) {
+                callback(element);
+            }
+        },
+
+        __load : function(what, where, callback) {
+            var xhr= new XMLHttpRequest();
+            xhr.open('GET', what, true);
+            xhr.onreadystatechange = function() {
+                if (this.readyState!==4) return;
+                if (this.status!==200) return; // or whatever error handling you want
+                if (callback!==undefined) {
+                    callback(this.responseText);
+                }
+            };
+            xhr.send();
+        }
+
+    }
 });
