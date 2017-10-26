9e8aa0870c94fe7c0910fccb8a8fa15f4d7b3d92
Simone <simone.castellani@nttdata.com>
2017-10-27 00:10:26 +0200

    manage masonry's layout of first items

--- scripts/app.js
+++ scripts/app.js
@@ -17,36 +17,44 @@ requirejs( [
     'imagesloaded/imagesloaded',
     'infinite-scroll/dist/infinite-scroll.pkgd',
     'app/utils',
+    'app/consts'
     ],
-    function( Masonry, imagesLoaded, InfiniteScroll, Utils ) {
+    function( Masonry, imagesLoaded, InfiniteScroll, Utils, consts ) {
 
         console.log('Init ...');
-
-        var container = document.getElementById("#container");
-
-        console.log('Load ...');
-        Utils._load("pages/index.html", "container");
+        var container = document.getElementById(consts.masonry.container);
 
         console.log('Init masonry ...');
-        var msnry = new Masonry( '#container', {
-            itemSelector: '.pagination__item', // select none at first
-            columnWidth: '.grid__col-sizer',
-            gutter: '.grid__gutter-sizer',
-            percentPosition: false,
+        var msnry = new Masonry( container, {
+            itemSelector: 'none', // select none at first
+            columnWidth: '.' + consts.masonry.columnWidth,
+            gutter: '.' + consts.masonry.gutter,
+            percentPosition: true,
             stagger: 100,
-            // nicer reveal transition
             visibleStyle: { opacity: 1 },
             hiddenStyle: { opacity: 0 },
+        });
+
+        console.log('Load ...');
+        Utils.__load("pages/index.html", container.id, function ( response ) {
+            Utils.__append(response, container.id, function ( element ) {
+                imagesLoaded( element, function() {
+                    msnry.options.itemSelector = "." + consts.infiniteScroll.appendSelector;
+                    var items = element.getElementsByClassName(consts.infiniteScroll.appendSelector);
+                    msnry.appended(items);
+                    msnry.layout();
+                });
+            });
         });
 
         // make imagesLoaded available for Infinite Scroll
         InfiniteScroll.imagesLoaded = imagesLoaded;
 
         console.log('Init infiniteScroll ...');
-        var infScroll = new InfiniteScroll( '#container', {
+        var infScroll = new InfiniteScroll( container, {
             // options...
             path: '/pages/{{#}}',
-            append: '.pagination__item',
+            append: "." + consts.infiniteScroll.appendSelector,
             status: '.page-load-status',
             hideNav: '.pagination',
             outlayer: msnry,
